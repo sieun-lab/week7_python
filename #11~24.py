d380ff5152a8a99a9d166b8ae23e59c0a5dae719
sieun <2401320@student.hywoman.ac.kr>
2025-10-21 20:10:30 +0900

    #16( 파일 이름 변경)

---
+++ #11~24.py
@@ -0,0 +1,46 @@
+#11_1
+user_input = input("임의의 숫자 5개를 입력하세요:")
+number_list = list(map(int, user_input.split()))
+print("입력받은 숫자 리스트:", number_list)
+
+#11_2
+user_input = input("임의의 숫자 5개를 입력하세요:")
+number_list = list(map(int, user_input.split()))
+char = input("리스트에 추가 할 문자 1개를 입력하세요:")
+number_list.append(char)
+print("업데이트 된 리스트:", number_list)
+
+#12
+user_input = =input("임의의 숫자 5개를 입력하세요:")
+number_list = list(map(int, user_input.split()))
+del number_list[-2:]
+print("마지막 두 개 값을 삭제:", number_list)
+
+#13
+user_input = =input("임의의 숫자 5개를 입력하세요:")
+number_list = list(map(int, user_input.split()))
+for index, value in enumerate(number_list, start=101):
+    print(f"{index}:{value}")
+
+#14
+a = [10, 20, 30, 40, 30, 20, 10]
+count_20 = a.count(20)
+print("숫자 20은 몇개인가요:", count_20)
+
+#15
+user_input = input("임의의 숫자 10개를 입력하세요:")
+number_list = list(map(int, user_input.split()))
+min_value = min(number_list)
+max_value = max(number_list)
+print("최저값:", min_value)
+print("최고값:", max_value)
+
+#16
+user_input = input("임의의 숫자 10개를 입력하세요:")
+number_list = list(map(int, user_input.split()))
+min_value = min(number_list)
+max_value = max(number_list)
+number_list.remove(min_value)
+number_list.remove(max_value)
+total = sum(number_list)
+print("최저값과 최고값을 제외한 나머지 값들의 합:", total)
